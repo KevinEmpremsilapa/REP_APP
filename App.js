d5c1bf305d5b1e1bee8b8e3a3a900ec3e4514911
Jacky <32426170+jacquelinesaelee@users.noreply.github.com>
2019-04-09 22:56:37 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-
 /*
  Navigation Page
 */
 import React from 'react';
 import {createDrawerNavigator, DrawerItems, createStackNavigator,StackNavigator,DrawerNavigator} from 'react-navigation';
-import { StyleSheet, Text, View, Button,SafeAreaView, ScrollView, Dimensions, Image,Asyncstorage} from 'react-native';
+import { Icon, StyleSheet, Text, View, Button,SafeAreaView, ScrollView, Dimensions, Image,Asyncstorage} from 'react-native';
+
+// Icons
+import defaultUserIcon from './Screens/Images/defaultUserIcon.png';
+import homeIcon from './Screens/Images/homeIconGray.png';
+import logoutIcon from './Screens/Images/logoutIcon.png';
 
 import Main from './Screens/Main';
-import HomeScreen from './Screens/UserScreens/HomeScreen';
-import Signup from './Screens/UserScreens/Signup';
-import SignupVendor from './Screens/VendorScreens/SignupVendor';
-import HomeScreenVendor from './Screens/VendorScreens/HomeScreenVendor';
-import UserEditProfile from './Screens/UserScreens/UserEditProfile';
+import HomeScreen from './Screens/HomeScreen';
+import Signup from './Screens/Signup';
+import SignupVendor from './Screens/SignupVendor';
+import HomeScreenVendor from './Screens/HomeScreenVendor';
 import styles from "./Screens/Styles";
-import VendorUser from './Screens/VendorUser';
+
+const repRed = '#FF6D6F';
+const repGray = '363636';
 
 
 export default class App extends React.Component{
@@ -24,17 +29,16 @@ export default class App extends React.Component{
      }
 }
 
-const CustomDrawerComponent = (props) => ( //customize the hamburger here //Medium well, no pickels 
+const CustomDrawerComponent = (props) => ( 
+    //customize the hamburger here
     <SafeAreaView style = {{flex:1}}>
-
         <View style = {styles.safeAreaStyle}>
-            <Image source = {require('./assets/Images/personIcon.png')} style={styles.menuImage}/>
+            <Image source = {defaultUserIcon} style={styles.userIconImage}/>
         </View>
 
-        <ScrollView>
-            <DrawerItems {...props}/>
+        <ScrollView >
+            <DrawerItems style={styles.contentOptions} {...props}/>
         </ScrollView>
-
     </SafeAreaView>
 )
 
@@ -43,47 +47,54 @@ const Stack = {
     HomeScreen: {screen: HomeScreen},
     SignupScreen: {screen: Signup},
     SignupVendor: {screen: SignupVendor},
-    HomeScreenVendor: {screen: HomeScreenVendor},
-    UserEditProfile: {screen: UserEditProfile},
-    VendorUser: {screen: VendorUser}
+    HomeScreenVendor: {screen: HomeScreenVendor}
 };
 
+
 const drawerRoutes = {
-   'Logout': {
+    'Logout': {
         name: 'Logout',
-        screen: createStackNavigator(Stack, {initialRouteName: 'MainScreen'})
+        screen: createStackNavigator(Stack, {initialRouteName: 'MainScreen'}),
+        navigationOptions:{
+            drawerIcon: <Image source={logoutIcon} style={[styles.drawerIconSize, styles.logoutIconSize]} />,
+        },
     },
     'Home': {
         name: 'Home',
-        screen: createStackNavigator(Stack, {initialRouteName: 'HomeScreen'})
+        screen: createStackNavigator(Stack, {initialRouteName: 'HomeScreen'}),
+        navigationOptions:{
+            drawerIcon: <Image source={homeIcon} style={styles.drawerIconSize}/>,
+        },
     },
     'Vendor Home': {
         name: 'Vendor Home',
-        screen: createStackNavigator(Stack, {initialRouteName: 'HomeScreenVendor'})
+        screen: createStackNavigator(Stack, {initialRouteName: 'HomeScreenVendor'}),
+        navigationOptions:{
+            drawerIcon: <Image source={homeIcon} style={styles.drawerIconSize}/>,
+        },
     },
-    'Edit Profile': {
-        name: 'Edit Profile',
-        screen: createStackNavigator(Stack, {initialRouteName: 'UserEditProfile'})
-    }
-
+    
+   
 };//creates paths for the drawer navigator using the stack navigation
-
 const RootNavigator = createStackNavigator({
     Drawer: {
         name: 'Drawer',
-        screen: createDrawerNavigator( 
+        screen: createDrawerNavigator(
            drawerRoutes,
             {
                 contentComponent: CustomDrawerComponent,
+                // Changed "navigationOpions" to "navigationOptions"
                 navigationOptions:{
                     drawerLockMode:'locked-closed' //should disable opening the drawer navigator by swiping right
+                 },
+                 contentOptions:{
+                    activeTintColor :'#FF6D6F',
                  }
             }
         )
     },
     ...Stack
 },
-
 {
     headerMode: 'none' //this removes the header for the stack navigator since we already have one for the drawer navigator
 });
